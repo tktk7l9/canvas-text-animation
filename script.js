ad197ee08f8ced28adfd7ab8ddc04a42eec318bf
tktk7l9 <saitotakuya0719@gmail.com>
2021-01-29 11:56:22 +0900

    Display Text Animation

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let particleArray = [];
+let adjustX = 30;
+let adjustY = 20;
 
 // handle mouse
 const mouse = {
     x: null,
     y: null,
-    radius: 250
+    radius: 150
 }
 
 window.addEventListener('mousemove', function (event) {
@@ -17,15 +19,15 @@ window.addEventListener('mousemove', function (event) {
 })
 
 ctx.fillStyle = 'white';
-ctx.font = '30px Verdana';
-ctx.fillText('A', 0, 30);
-const data = ctx.getImageData(0, 0, 100, 100);
+ctx.font = '10px Verdana';
+ctx.fillText('Takuya Saito', 0, 15);
+const textCoordinates = ctx.getImageData(0, 0, 100, 100);
 
 class Particle {
     constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.size = 3;
+        this.size = 1;
         this.baseX = this.x;
         this.baseY = this.y;
         this.density = (Math.random() * 40) + 5;
@@ -65,10 +67,14 @@ class Particle {
 
 function init() {
     particleArray = [];
-    for (let i = 0; i < 1000; i++){
-        let x = Math.random() * canvas.width;
-        let y = Math.random() * canvas.height;
-        particleArray.push(new Particle(x, y));
+    for (let y = 0, y2 = textCoordinates.height; y < y2; y++){
+        for (let x = 0, x2 = textCoordinates.width; x < x2; x++){
+            if (textCoordinates.data[(y * 4 * textCoordinates.width) + (x * 4) + 3] > 128) {
+                let positionX = x + adjustX;
+                let positionY = y + adjustY;
+                particleArray.push(new Particle(positionX * 15, positionY * 15));
+            }
+        }
     }
 }
 init();
